31ac3cb8b21443faac15d5360bdea9d61f609f28
Raushan kumar <86125144+raushan9jnv@users.noreply.github.com>
2023-01-26 17:46:06 +0530

    final predication code, predication on single value working, prediction_original not proper only

--- GenericTextClassification/TextClassification.py
+++ GenericTextClassification/TextClassification.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
@@ -21,9 +22,10 @@ class TextClassificationModel:
 
     def clean_dataset(self):
         # perform any necessary cleaning of the dataset here
+
         # Extract categorical and numeric columns
         categorical_cols = dataframe.select_dtypes(include=['object']).columns
-        numeric_cols = dataframe.select_dtypes(include=['float64','int64']).columns
+        numeric_cols = dataframe.select_dtypes(include=['float64', 'int64']).columns
 
         # Handling null values for categorical columns
         for col in categorical_cols:
@@ -33,26 +35,23 @@ class TextClassificationModel:
         for col in numeric_cols:
             dataframe[col].fillna(dataframe[col].mean(), inplace=True)
 
-
     def feature_segregation(self):
-        # The error message "Found input variables with inconsistent numbers of samples" is occurring because the number of rows in the multiple text columns you passed in as text_columns is different from the number of rows in the categorical column you passed in as categorical_column.
-        # One solution for this is to concatenate the multiple text columns into a single column by using the apply function from pandas and passing in a lambda function that concatenates the values of the two text columns together with a separator in between.
-        if len(self.text_columns)>1:
-            self.df["text"] = self.df[self.text_columns].apply(lambda x: " ".join(x), axis=1)
+        if len(self.text_columns) > 1:
+            self.df["text"] = self.df[self.text_columns].apply(
+                lambda x: " ".join(x), axis=1)
         else:
-            self.df["text"] = self.df[self.text_columns[0]] 
+            self.df["text"] = self.df[self.text_columns[0]]
         self.X = self.df["text"]
         self.y = self.df[self.categorical_column]
-        
-        #save original lables
-        self.label_mapping = dict(enumerate(self.y.unique()))
-        
+
     def feature_extraction(self):
         self.vectorizer = TfidfVectorizer()
         self.X = self.vectorizer.fit_transform(self.X)
 
     def train_model(self):
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.2)
+        self.classes_ = np.unique(self.y)
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+            self.X, self.y, test_size=0.2)
 
         self.models = {
             'Logistic Regression': LogisticRegression(),
@@ -80,52 +79,67 @@ class TextClassificationModel:
         with open(filename, 'rb') as file:
             self.best_model = pickle.load(file)
 
-    # def predict(self, text):
-    #     text = self.vectorizer.transform([text])
-    #     predictions = self.best_model.predict(text)
+    def predict(self, data):
+        data = self.vectorizer.transform(data)
+        predictions = self.best_model.predict(data)
+        return predictions
 
-    #     # if self.categorical_column not in self.df.columns:
-    #     #     print("Error: column name not found in the dataframe.")
-    #     # else:
-    #     #     print('success')
-    #     # drop rows with missing values
-    #     text = text.copy()
-    #     # text.dropna(subset=[self.categorical_column], inplace=True)
-    #     # dataframe.dropna(subset=[self.categorical_column], inplace=True)
-            
-    #     original_labels = [self.label_mapping[label] for label in predictions]
-    #     return original_labels
+    def predict_original(self, text_data):
+        text_data = self.vectorizer.transform(text_data)
+        predictions = self.best_model.predict(text_data)
+        # print("################",self.X.shape)
+        # print("################",text_data.shape)
 
-    def predict(self, text):
-        text = self.vectorizer.transform([text])
-        return self.best_model.predict(text)
+        if self.X.shape != text_data.shape:
+            raise ValueError("Input data shape does not match the shape of the data used to train the model.")
+        original_predictions = self.vectorizer.inverse_transform(predictions)
+        return original_predictions
 
 
-
-# df = pd.read_csv("data.csv")
-# model = TextClassificationModel(df, ["Ticket_Title", "Application"], "Master_SOP")
-# model.show_summary()
-# model.clean_dataset()
-# model.feature_segregation()
-# model.feature_extraction()
-# model.train_model()
-# model.save
 
 dataframe = pd.read_csv("data.csv")
-text_classification_model = TextClassificationModel(dataframe, ["Ticket_Title", "Application"], "Master_SOP")
+text_classification_model = TextClassificationModel(
+    dataframe, ["Ticket_Title", "Application"], "Master_SOP")
 # text_classification_model.show_summary()
 text_classification_model.clean_dataset()
 text_classification_model.feature_segregation()
 text_classification_model.feature_extraction()
 text_classification_model.train_model()
-text_classification_model.save_model("best_model.pkl")
-print('model saved')
 
+# text_data = dataframe[["Ticket_Title", "Application"]].apply(lambda x: " ".join(x), axis=1)
+# predictions = text_classification_model.predict(text_data)
+# print(predictions)
 
-# print(dataframe.columns)
-# Apply the predict function to the DataFrame
-# dataframe["predictions"] = text_classification_model.predict(dataframe["text"].to_string())
+# Add the predictions as a new column in the dataframe
+# dataframe['predicted_class'] = predictions
+# print(dataframe['predicted_class'])
 
-# Print the results
-# print(dataframe)
+# text_data = dataframe[["Ticket_Title", "Application"]].apply(lambda x: " ".join(x), axis=1)
+# text_data = text_data.tolist()
+# print(type(text_data))
+# original_predictions = text_classification_model.predict_original(text_data)
+# print(original_predictions)
+
+# text_classification_model.predict(dataframe)
+# text_classification_model.save_model("best_model.pkl")
+print('completed')
+
+#PREDICT ON a single text data row with two column
+
+import pandas as pd
+
+# create a dataframe with two columns 'Ticket_Title' and 'Application'
+data = {'Ticket_Title': ['JDE Incident Request', 'Failed payment batches: JDE to Trax- V2 to V3 folder'],
+        'Application': ['GRP-25 JDE EMEA', 'GRP-25 JDE EMEA']}
+df = pd.DataFrame(data)
+
+# concatenate the values of the two columns
+df['text_data'] = df['Ticket_Title'] + ' ' + df['Application']
+
+# predict on the 'text_data' column
+predictions = text_classification_model.predict(df['text_data'])
+
+# add the predictions as a new column in the dataframe
+df['predictions'] = predictions
+print(df['predictions'])
 
